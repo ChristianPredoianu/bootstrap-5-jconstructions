56ff2f6957a585eb23461a75788f88e137122b8f
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2021-07-20 13:01:58 +0200

    final version

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import * as bootstrap from 'bootstrap';
 import '@fortawesome/fontawesome-free/css/solid.css';
+import '@fortawesome/fontawesome-free/css/brands.css';
 import '@fortawesome/fontawesome-free/css/fontawesome.css';
